3739ed0736e26431ff0786f8aee87b39d97be9a7
nilshaebel <nils.haebel@bitsandmachines.com>
2019-05-01 14:44:59 +0200

    Improved macOS configuration

--- setup/macos-initial-setup.sh
+++ setup/macos-initial-setup.sh
@@ -1,9 +1,45 @@
 # increase pointer speed for an external mouse
 defaults write -g com.apple.mouse.scaling 5.0
 
-# show expanded safe dialogue by default
+# show expanded safe dialog by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# show expanded print dialog by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# enable key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# increase keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+# use local drive instead of iCloud by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# disable automatic capitalization
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# disable "smart" dashes
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# disable automatic period substitution
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# disable "smart" quotes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# disable shadows for screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# enable subpixel font rendering on external (non-Apple) screens
+defaults write NSGlobalDomain AppleFontSmoothing -int 1
+
 
 
 #
@@ -12,6 +48,7 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
 # show file extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
 
 
 #
